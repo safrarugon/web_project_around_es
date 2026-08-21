5111e19c323f18a4880a12ff9d21149bf97f83bb
safrarugon <safraugon1983@gmail.com>
2026-08-21 00:52:22 -0600

    Etapa 2 1er entrega

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,49 @@
-let initialCards = [
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileEditPopup = document.querySelector("#edit-popup");
+const profileEditCloseBtn = profileEditPopup.querySelector(".popup__close");
+const profileEditForm = profileEditPopup.querySelector("#edit-profile-form");
+const profileEditTitle = profileEditForm.querySelector(
+  ".popup__input_type_name",
+);
+const profileEditDescription = profileEditForm.querySelector(
+  ".popup__input_type_description",
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+function fillProfileForm() {
+  profileEditTitle.setAttribute("value", profileTitle.textContent);
+  profileEditDescription.setAttribute("value", profileDescription.textContent);
+}
+function handleOpenEditModal() {
+  openModal(profileEditPopup);
+  fillProfileForm();
+}
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profileTitle.textContent = profileEditTitle.value;
+  profileDescription.textContent = profileEditDescription.value;
+}
+
+profileEditBtn.addEventListener("click", () => {
+  handleOpenEditModal();
+});
+
+profileEditCloseBtn.addEventListener("click", () => {
+  closeModal(profileEditPopup);
+});
+profileEditForm.addEventListener("submit", () => {
+  handleProfileFormSubmit(event);
+  closeModal(profileEditPopup);
+});
+
+const initialCards = [
   {
     name: "Valle de Yosemite",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
