4b6e789a00d1b3f7b3ef21e1c2b1ebc7fc1e80b6
safrarugon <safraugon1983@gmail.com>
2026-09-16 23:53:12 -0600

    Cambios de la revision del proyecto

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,51 +1,58 @@
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
-//---------------Funciones de validación de formularios y mensajes de error------------------------------
-//Funcion para mostrar el mensaje de error en el input correspondiente-----------------------------------
+//---Funciones de validación de formulario y botónes submit
+//Función para mostrar el mensaje de error en el input correspondiente
 function showInputError(form, input) {
-  //Obtiene el elemento de error correspondiente al input------------------------------------------------
+  //Obtiene el elemento de error correspondiente al input
   const errorElement = form.querySelector(`.${input.name}-error`);
-  //Validación del input y muestra el mensaje de error si no es válido-----------------------------------
+  //Validación del input y muestra el mensaje de error si no es válido
   if (!input.validity.valid) {
-    //Muestra el mensaje de error en el elemento correspondiente-----------------------------------------
+    //Muestra el mensaje de error en el elemento correspondiente
     errorElement.textContent = input.validationMessage;
     input.classList.add("popup__input_invalid");
   } else {
-    //Si el input es válido, limpia el mensaje de error--------------------------------------------------
+    //Si el input es válido, limpia el mensaje de error
     errorElement.textContent = "";
     input.classList.remove("popup__input_invalid");
   }
 }
-//Función para verificar la validez del formulario y habilitar/deshabilitar el botón de envío------------
-function setEventListeners(form, submitButton) {
-  //Obtiene todos los inputs del formulario--------------------------------------------------------------
-  const inputs = form.querySelectorAll(".popup__input");
-  //Itera sobre cada input y muestra el mensaje de error correspondiente---------------------------------
-  inputs.forEach((input) => {
-    //Llama a la función para mostrar el mensaje de error------------------------------------------------
-    showInputError(form, input);
-  });
-  //Habilita o deshabilita el botón de envío según la validez del formulario-----------------------------
+//Función para habilitar o deshabilitar el botón de envío según la validez del formulario
+function toggleButtonState(form, submitButton) {
+  //Habilita o deshabilita el botón de envío según la validez del formulario
   submitButton.disabled = !form.checkValidity();
 }
-//Función para resetear los errores de validación en el formulario---------------------------------------
-function resetValidation(form) {
-  //Obtiene todos los inputs del formulario--------------------------------------------------------------
+
+//---Funciones llamadas desde Index.js
+//Función para validar formularios y estado del botón submit
+function setEventListeners(form, submitButton) {
+  //Obtiene todos los inputs del formulario
   const inputs = form.querySelectorAll(".popup__input");
-  //Itera sobre cada input-------------------------------------------------------------------------------
+  //Itera sobre cada input y muestra el mensaje de error correspondiente
   inputs.forEach((input) => {
-    //Quita la clase de error del input------------------------------------------------------------------
+    //Llama a la función para mostrar el mensaje de error
+    showInputError(form, input);
+  });
+  //Llama a la funcion para habilitar o deshabilitar el botón de envío según la validez del formulario
+  toggleButtonState(form, submitButton);
+}
+//Función para resetear los errores de validación en el formulario
+function resetValidation(form) {
+  //Obtiene todos los inputs del formulario
+  const inputs = form.querySelectorAll(".popup__input");
+  //Itera sobre cada input
+  inputs.forEach((input) => {
+    //Quita la clase de error del input
     input.classList.remove("popup__input_invalid");
-    //Obtiene el elemento de error correspondiente al input-----------------------------------------------
+    //Obtiene el elemento de error correspondiente al input
     const errorElement = form.querySelector(`.${input.name}-error`);
-    //Valida si el elemento de error existe---------------------------------------------------------------
+    //Valida si el elemento de error existe
     if (errorElement) {
-      //Limpia el mensaje de error del elemento----------------------------------------------------------
+      //Limpia el mensaje de error del elemento
       errorElement.textContent = "";
     }
+    //Llama a la función para habilitar o deshabilitar el botón de envío según la validez del formulario
+    const submitButton = form.querySelector(".popup__button");
+    toggleButtonState(form, submitButton);
   });
 }
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
-//---------------Exportación de funciones para validación de formularios---------------------------------
+
+//Exportación de funciones para validación de formularios
 export { setEventListeners, resetValidation };
